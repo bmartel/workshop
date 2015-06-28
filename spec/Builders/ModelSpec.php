7a075895e2f36e1cc8df92f187a4ce6b8a44bfec
Brandon Martel <brandonmartel@gmail.com>
2015-06-28 16:33:02 -0500

    updating test for changes to default file namespacing

--- spec/Builders/ModelSpec.php
+++ spec/Builders/ModelSpec.php
@@ -13,7 +13,7 @@ class ModelSpec extends ObjectBehavior
 
     function it_can_generate_a_namespace_and_path_to_house_models()
     {
-        $this->getNamespaceAndPathForType('Thing')->shouldReturn(['Bmartel\\Workshop\\Model', 'src/Model/Thing.php']);
+        $this->getNamespaceAndPathForType('Thing')->shouldReturn(['Bmartel\\Workshop\\Models', 'src/Models/Thing.php']);
     }
 
     function it_can_generate_a_namespace_and_path_to_house_models_with_override()
